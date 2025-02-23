d9d18e27ac305904e133e0bf195e895efabc389b
Tsuki <TsukiMaaii@gmail.com>
2025-02-23 18:04:34 +0800

    chore 1.添加虚拟权限功能 2.添加查询玩家UUID指令 3.添加查询home与stage详情指令 4.优化Component部分实现
    (cherry picked from commit 1cbc8d3c2caeb60b82570c5146bd21a856adf162)

--- src/main/java/xin/vanilla/narcissus/enums/ECommandType.java
+++ src/main/java/xin/vanilla/narcissus/enums/ECommandType.java
@@ -4,62 +4,91 @@ import lombok.Getter;
 
 @Getter
 public enum ECommandType {
-    HELP(0),
-    DIMENSION(0),
-    DIMENSION_CONCISE(0),
-    FEED(0),
-    FEED_CONCISE(0),
-    TP_COORDINATE(1),
-    TP_COORDINATE_CONCISE(2),
-    TP_STRUCTURE(3),
-    TP_STRUCTURE_CONCISE(4),
-    TP_ASK(5),
-    TP_ASK_CONCISE(6),
-    TP_ASK_YES(7),
-    TP_ASK_YES_CONCISE(8),
-    TP_ASK_NO(9),
-    TP_ASK_NO_CONCISE(10),
-    TP_HERE(11),
-    TP_HERE_CONCISE(12),
-    TP_HERE_YES(13),
-    TP_HERE_YES_CONCISE(14),
-    TP_HERE_NO(15),
-    TP_HERE_NO_CONCISE(16),
-    TP_RANDOM(17),
-    TP_RANDOM_CONCISE(18),
-    TP_SPAWN(19),
-    TP_SPAWN_CONCISE(20),
-    TP_WORLD_SPAWN(21),
-    TP_WORLD_SPAWN_CONCISE(22),
-    TP_TOP(23),
-    TP_TOP_CONCISE(24),
-    TP_BOTTOM(25),
-    TP_BOTTOM_CONCISE(26),
-    TP_UP(27),
-    TP_UP_CONCISE(28),
-    TP_DOWN(29),
-    TP_DOWN_CONCISE(30),
-    TP_VIEW(31),
-    TP_VIEW_CONCISE(32),
-    TP_HOME(33),
-    TP_HOME_CONCISE(34),
-    SET_HOME(35),
-    SET_HOME_CONCISE(36),
-    DEL_HOME(37),
-    DEL_HOME_CONCISE(38),
-    TP_STAGE(39),
-    TP_STAGE_CONCISE(40),
-    SET_STAGE(41),
-    SET_STAGE_CONCISE(42),
-    DEL_STAGE(43),
-    DEL_STAGE_CONCISE(44),
-    TP_BACK(45),
-    TP_BACK_CONCISE(46);
+    HELP(false, false),
+    UUID(),
+    UUID_CONCISE(),
+    DIMENSION(),
+    DIMENSION_CONCISE(),
+    FEED(),
+    FEED_OTHER(true),
+    FEED_CONCISE(),
+    FEED_OTHER_CONCISE(true),
+    TP_COORDINATE(),
+    TP_COORDINATE_CONCISE(),
+    TP_STRUCTURE(),
+    TP_STRUCTURE_CONCISE(),
+    TP_ASK(),
+    TP_ASK_CONCISE(),
+    TP_ASK_YES(false, false),
+    TP_ASK_YES_CONCISE(),
+    TP_ASK_NO(false, false),
+    TP_ASK_NO_CONCISE(),
+    TP_HERE(),
+    TP_HERE_CONCISE(),
+    TP_HERE_YES(false, false),
+    TP_HERE_YES_CONCISE(),
+    TP_HERE_NO(false, false),
+    TP_HERE_NO_CONCISE(),
+    TP_RANDOM(),
+    TP_RANDOM_CONCISE(),
+    TP_SPAWN(),
+    TP_SPAWN_OTHER(true),
+    TP_SPAWN_CONCISE(),
+    TP_SPAWN_OTHER_CONCISE(true),
+    TP_WORLD_SPAWN(),
+    TP_WORLD_SPAWN_CONCISE(),
+    TP_TOP(),
+    TP_TOP_CONCISE(),
+    TP_BOTTOM(),
+    TP_BOTTOM_CONCISE(),
+    TP_UP(),
+    TP_UP_CONCISE(),
+    TP_DOWN(),
+    TP_DOWN_CONCISE(),
+    TP_VIEW(),
+    TP_VIEW_CONCISE(),
+    TP_HOME(),
+    TP_HOME_CONCISE(),
+    SET_HOME(false, false),
+    SET_HOME_CONCISE(),
+    DEL_HOME(false, false),
+    DEL_HOME_CONCISE(),
+    GET_HOME(false, false),
+    GET_HOME_CONCISE(),
+    TP_STAGE(),
+    TP_STAGE_CONCISE(),
+    SET_STAGE(),
+    SET_STAGE_CONCISE(),
+    DEL_STAGE(),
+    DEL_STAGE_CONCISE(),
+    GET_STAGE(),
+    GET_STAGE_CONCISE(),
+    TP_BACK(),
+    TP_BACK_CONCISE(),
+    VIRTUAL_OP(),
+    VIRTUAL_OP_CONCISE();
 
-    private final int sort;
+    private final boolean ignore;
+    private final boolean concise = this.name().endsWith("_CONCISE");
+    private final boolean op;
 
-    ECommandType(int sort) {
-        this.sort = sort;
+    ECommandType() {
+        this.ignore = false;
+        this.op = !this.concise;
+    }
+
+    ECommandType(boolean ig) {
+        this.ignore = ig;
+        this.op = !this.concise;
+    }
+
+    ECommandType(boolean ig, boolean op) {
+        this.ignore = ig;
+        this.op = !this.concise && op;
+    }
+
+    public int getSort() {
+        return this.ordinal();
     }
 
     public ECommandType replaceConcise() {
