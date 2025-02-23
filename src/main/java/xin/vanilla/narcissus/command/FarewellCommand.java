649758e578d61d37f6bdb1e6db51a910c8918a9e
Tsuki <TsukiMaaii@gmail.com>
2025-02-24 00:38:56 +0800

    chore 1.查询home、stage时不同维度换行显示

--- src/main/java/xin/vanilla/narcissus/command/FarewellCommand.java
+++ src/main/java/xin/vanilla/narcissus/command/FarewellCommand.java
@@ -874,7 +874,7 @@ public class FarewellCommand {
                         dimension.append(name);
                         dimension.append(Component.literal(", ").setColor(EMCColor.GRAY.getColor()));
                     }
-                    info.append(dimension);
+                    info.append(dimension).append("\n");
                 }
                 component = Component.translatable(language, EI18nType.MESSAGE, "home_is", info);
             }
@@ -1023,7 +1023,7 @@ public class FarewellCommand {
                         dimension.append(name);
                         dimension.append(Component.literal(", ").setColor(EMCColor.GRAY.getColor()));
                     }
-                    info.append(dimension);
+                    info.append(dimension).append("\n");
                 }
                 component = Component.translatable(language, EI18nType.MESSAGE, "stage_is", info);
             }
