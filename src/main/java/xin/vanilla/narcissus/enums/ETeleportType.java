a90134f2ca3141e6f66227a2e9da0d90f9b949bf
Tsuki <TsukiMaaii@gmail.com>
2025-02-24 17:00:52 +0800

    chore 1.添加检测更新 2.安全传送改为异步执行 3.添加安全传送目标点查询范围配置
    fix
    1.feed指令无法通过配置文件禁用

--- src/main/java/xin/vanilla/narcissus/enums/ETeleportType.java
+++ src/main/java/xin/vanilla/narcissus/enums/ETeleportType.java
@@ -17,5 +17,42 @@ public enum ETeleportType {
     TP_STAGE,
     TP_BACK,
     DEATH,
-    OTHER,
+    OTHER;
+
+    public ECommandType toCommandType() {
+        switch (this) {
+            case TP_COORDINATE:
+                return ECommandType.TP_COORDINATE;
+            case TP_STRUCTURE:
+                return ECommandType.TP_STRUCTURE;
+            case TP_ASK:
+                return ECommandType.TP_ASK;
+            case TP_HERE:
+                return ECommandType.TP_HERE;
+            case TP_RANDOM:
+                return ECommandType.TP_RANDOM;
+            case TP_SPAWN:
+                return ECommandType.TP_SPAWN;
+            case TP_WORLD_SPAWN:
+                return ECommandType.TP_WORLD_SPAWN;
+            case TP_TOP:
+                return ECommandType.TP_TOP;
+            case TP_BOTTOM:
+                return ECommandType.TP_BOTTOM;
+            case TP_UP:
+                return ECommandType.TP_UP;
+            case TP_DOWN:
+                return ECommandType.TP_DOWN;
+            case TP_VIEW:
+                return ECommandType.TP_VIEW;
+            case TP_HOME:
+                return ECommandType.TP_HOME;
+            case TP_STAGE:
+                return ECommandType.TP_STAGE;
+            case TP_BACK:
+                return ECommandType.TP_BACK;
+            default:
+                return null;
+        }
+    }
 }
