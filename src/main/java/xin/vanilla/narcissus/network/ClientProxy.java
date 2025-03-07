c276bb5c5ca2a09806042042d458a9ee6a2adc43
Tsuki <TsukiMaaii@gmail.com>
2025-03-07 22:27:04 +0800

    chore 1.添加传送音效 2.重构capability包名为data

--- src/main/java/xin/vanilla/narcissus/network/ClientProxy.java
+++ src/main/java/xin/vanilla/narcissus/network/ClientProxy.java
@@ -4,7 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xin.vanilla.narcissus.capability.player.PlayerTeleportDataCapability;
+import xin.vanilla.narcissus.data.player.PlayerTeleportDataCapability;
 
 public class ClientProxy {
     public static final Logger LOGGER = LogManager.getLogger();
