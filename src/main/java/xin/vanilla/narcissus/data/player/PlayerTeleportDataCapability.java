700bc2740be06d640e0fbcf030cc34f391dd4fe9
Tsuki <TsukiMaaii@gmail.com>
2025-03-07 22:27:04 +0800

    chore 1.添加传送音效 2.重构capability包名为data

--- src/main/java/xin/vanilla/narcissus/data/player/PlayerTeleportDataCapability.java
+++ src/main/java/xin/vanilla/narcissus/data/player/PlayerTeleportDataCapability.java
@@ -1,4 +1,4 @@
-package xin.vanilla.narcissus.capability.player;
+package xin.vanilla.narcissus.data.player;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
