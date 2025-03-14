cc6bc9f03f6be575072deade903f1f8629e50eb3
Tsuki <TsukiMaaii@gmail.com>
2025-03-15 00:56:31 +0800

    fix 1.寻找被吸引的实体
    chore
    1.更新配置文件示例

--- src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
+++ src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
@@ -1245,7 +1245,11 @@ public class NarcissusUtils {
             // 排除敌对生物
             if (entity instanceof MonsterEntity) continue;
 
-            if ((entity.getTarget() == player) || entity.goalSelector.getRunningGoals().anyMatch(goal -> goal.getGoal() instanceof TemptGoal)) {
+            if (entity.goalSelector.getRunningGoals()
+                    .anyMatch(goal -> goal.isRunning()
+                            && (goal.getGoal() instanceof TemptGoal)
+                            && FieldUtils.getPrivateFieldValue(TemptGoal.class, goal.getGoal(), FieldUtils.getTemptGoalPlayerFieldName()) == player
+                    )) {
                 doTeleport(entity, coordinate, level);
             }
         }
