c53f75ab966d83327e5d6eec0718d5dfe66c1caa
Tsuki <TsukiMaaii@gmail.com>
2025-06-23 20:04:25 +0800

    fix 1.坐标计算的错误

--- src/main/java/xin/vanilla/narcissus/enums/EnumSafeMode.java
+++ src/main/java/xin/vanilla/narcissus/enums/EnumSafeMode.java
@@ -2,7 +2,24 @@ package xin.vanilla.narcissus.enums;
 
 public enum EnumSafeMode {
     NONE,
-    Y_UP,
-    Y_DOWN,
-    Y_OFFSET_3,
+    /**
+     * 当前位置到顶部
+     */
+    Y_C_TO_T,
+    /**
+     * 底部到当前位置
+     */
+    Y_B_TO_C,
+    /**
+     * 当前位置到底部
+     */
+    Y_C_TO_B,
+    /**
+     * 顶部到当前位置
+     */
+    Y_T_TO_C,
+    /**
+     * 当前位置+-3
+     */
+    Y_C_OFFSET_3,
 }
