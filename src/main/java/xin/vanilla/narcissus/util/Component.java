d64ca21fa52c010dfff062bd8cf75c61eff4ef4d
Tsuki <TsukiMaaii@gmail.com>
2025-07-21 12:10:27 +0800

    chore 1.更新默认配置文件示例 2.修改物品代价不足提示 3.优化自定义消息组件
    (cherry picked from commit bd9e37c05d54c20e9f905d29bcb37ac7386d69a3)

--- src/main/java/xin/vanilla/narcissus/util/Component.java
+++ src/main/java/xin/vanilla/narcissus/util/Component.java
@@ -161,6 +161,13 @@ public class Component implements Cloneable, Serializable {
         return this;
     }
 
+    public Component setLanguageCodeIfEmpty(String languageCode) {
+        if (this.isLanguageCodeEmpty()) {
+            this.setLanguageCode(languageCode);
+        }
+        return this;
+    }
+
     // region NonNull Getter
 
     /**
@@ -595,7 +602,8 @@ public class Component implements Cloneable, Serializable {
                         i++;
                     }
                 } else {
-                    components.add(new StringTextComponent(this.text).withStyle(this.getStyle()));
+                    this.args.forEach(arg -> arg.setLanguageCodeIfEmpty(languageCode));
+                    components.add(new StringTextComponent(StringUtils.format(this.text, this.args.toArray())).withStyle(this.getStyle()));
                 }
             }
         }
