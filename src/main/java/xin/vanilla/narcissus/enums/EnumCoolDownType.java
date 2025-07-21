a5cdc4d057803df13a74af7eab3be56a8f8be075
Tsuki <TsukiMaaii@gmail.com>
2025-07-21 12:10:27 +0800

    chore 1.更新默认配置文件示例 2.修改物品代价不足提示 3.优化自定义消息组件

--- src/main/java/xin/vanilla/narcissus/enums/EnumCoolDownType.java
+++ src/main/java/xin/vanilla/narcissus/enums/EnumCoolDownType.java
@@ -1,7 +1,17 @@
 package xin.vanilla.narcissus.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum EnumCoolDownType {
     COMMON,
     INDIVIDUAL,
     MIXED,
+    ;
+
+
+    public static List<String> names() {
+        return Arrays.stream(EnumCoolDownType.values()).map(Enum::name).collect(Collectors.toList());
+    }
 }
