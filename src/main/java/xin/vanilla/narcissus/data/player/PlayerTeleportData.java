29ba9c5a1470fb047221cf7dc897541385d0ae55
Tsuki <TsukiMaaii@gmail.com>
2025-10-02 15:59:50 +0800

    feat 1.移除PlayerCapability并改为自定义PlayerDataManager
    (cherry picked from commit 30394ff37664a9c1edb1832de0b64547c37b46a4)

--- src/main/java/xin/vanilla/narcissus/data/player/PlayerTeleportData.java
+++ src/main/java/xin/vanilla/narcissus/data/player/PlayerTeleportData.java
@@ -1,20 +1,20 @@
 package xin.vanilla.narcissus.data.player;
 
-import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import xin.vanilla.narcissus.config.ServerConfig;
+import net.minecraft.world.entity.player.Player;
 import xin.vanilla.narcissus.data.Coordinate;
 import xin.vanilla.narcissus.data.KeyValue;
 import xin.vanilla.narcissus.data.PlayerAccess;
 import xin.vanilla.narcissus.data.TeleportRecord;
 import xin.vanilla.narcissus.enums.EnumTeleportType;
+import xin.vanilla.narcissus.network.packet.PlayerDataSyncPacket;
 import xin.vanilla.narcissus.util.CollectionUtils;
 import xin.vanilla.narcissus.util.DateUtils;
+import xin.vanilla.narcissus.util.NarcissusUtils;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -23,155 +23,44 @@ import java.util.stream.Collectors;
 /**
  * 玩家传送数据
  */
-public class PlayerTeleportData implements IPlayerTeleportData {
-    /**
-     * 是否已发送使用说明
-     */
-    @Getter
-    @Setter
-    private boolean notified;
-    private Date lastCardTime;
-    private Date lastTpTime;
-    private final AtomicInteger teleportCard = new AtomicInteger();
-    private List<TeleportRecord> teleportRecords;
-    /**
-     * dimension:name coordinate
-     */
-    private Map<KeyValue<String, String>, Coordinate> homeCoordinate;
-    /**
-     * dimension:name
-     */
-    private Map<String, String> defaultHome;
-    /**
-     * 玩家自定义的黑白名单
-     */
-    private PlayerAccess access;
+public final class PlayerTeleportData implements IPlayerData<PlayerTeleportData> {
 
-    @Override
-    public int getTeleportCard() {
-        return this.teleportCard.get();
-    }
+    // region override
 
-    @Override
-    public int plusTeleportCard() {
-        return this.teleportCard.incrementAndGet();
-    }
+    private static final Map<UUID, PlayerTeleportData> CACHE = Collections.synchronizedMap(new WeakHashMap<>());
+    private final Player player;
+    private boolean dirty = false;
 
-    @Override
-    public int plusTeleportCard(int num) {
-        return this.teleportCard.addAndGet(num);
-    }
-
-    @Override
-    public int subTeleportCard() {
-        return this.teleportCard.decrementAndGet();
-    }
-
-    @Override
-    public int subTeleportCard(int num) {
-        return this.teleportCard.addAndGet(-num);
-    }
-
-    @Override
-    public void setTeleportCard(int num) {
-        this.teleportCard.set(num);
-    }
-
-    @Override
-    public @NonNull Date getLastCardTime() {
-        return this.lastCardTime = this.lastCardTime == null ? DateUtils.getDate(0, 1, 1) : this.lastCardTime;
-    }
-
-    @Override
-    public void setLastCardTime(Date time) {
-        this.lastCardTime = time;
-    }
-
-    @Override
-    public @NonNull Date getLastTpTime() {
-        return this.lastTpTime = this.lastTpTime == null ? DateUtils.getDate(0, 1, 1) : this.lastTpTime;
-    }
-
-    @Override
-    public void setLastTpTime(Date time) {
-        this.lastTpTime = time;
-    }
-
-    @Override
-    public @NonNull List<TeleportRecord> getTeleportRecords() {
-        return this.teleportRecords = CollectionUtils.isNullOrEmpty(this.teleportRecords) ? new ArrayList<>() : this.teleportRecords;
-    }
-
-    @Override
-    public @NonNull List<TeleportRecord> getTeleportRecords(EnumTeleportType type) {
-        return CollectionUtils.isNullOrEmpty(this.teleportRecords) ? this.teleportRecords = new ArrayList<>() :
-                this.teleportRecords.stream().filter(record -> record.getTeleportType() == type).collect(Collectors.toList());
-    }
-
-    @Override
-    public void setTeleportRecords(List<TeleportRecord> records) {
-        this.teleportRecords = records;
-    }
-
-    @Override
-    public void addTeleportRecords(TeleportRecord... records) {
-        this.getTeleportRecords().addAll(Arrays.asList((records)));
-        this.getTeleportRecords().sort(Comparator.comparing(TeleportRecord::getTeleportTime));
-        int limit = ServerConfig.TELEPORT_RECORD_LIMIT.get();
-        int size = this.getTeleportRecords().size();
-        if (limit > 0 && limit < size) {
-            this.getTeleportRecords().subList(0, size - limit).clear();
+    private PlayerTeleportData(Player player) {
+        this.player = player;
+        if (this.player instanceof ServerPlayer) {
+            this.deserializeNBT(PlayerDataManager.instance().getOrCreate(player), false);
         }
     }
 
-    @Override
-    public Map<KeyValue<String, String>, Coordinate> getHomeCoordinate() {
-        return this.homeCoordinate = this.homeCoordinate == null ? new HashMap<>() : this.homeCoordinate;
+    /**
+     * 获取或创建 PlayerTeleportData
+     */
+    public static PlayerTeleportData getData(Player player) {
+        return CACHE.computeIfAbsent(player.getUUID(), k -> new PlayerTeleportData(player));
     }
 
     @Override
-    public void setHomeCoordinate(Map<KeyValue<String, String>, Coordinate> homeCoordinate) {
-        this.homeCoordinate = homeCoordinate;
+    public boolean isDirty() {
+        return this.dirty;
     }
 
     @Override
-    public void addHomeCoordinate(KeyValue<String, String> key, Coordinate coordinate) {
-        this.getHomeCoordinate().put(key, coordinate);
+    public void setDirty() {
+        this.dirty = true;
     }
 
     @Override
-    public Map<String, String> getDefaultHome() {
-        return this.defaultHome = this.defaultHome == null ? new HashMap<>() : this.defaultHome;
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
     }
 
     @Override
-    public void setDefaultHome(Map<String, String> defaultHome) {
-        this.defaultHome = defaultHome;
-    }
-
-    @Override
-    public void addDefaultHome(String key, String value) {
-        this.getDefaultHome().put(key, value);
-    }
-
-    @Override
-    public KeyValue<String, String> getDefaultHome(String dimension) {
-        if (this.getDefaultHome().containsKey(dimension)) {
-            return new KeyValue<>(dimension, this.getDefaultHome().get(dimension));
-        }
-        return null;
-    }
-
-    @Override
-    public PlayerAccess getAccess() {
-        return this.access = this.access == null ? new PlayerAccess() : this.access;
-    }
-
-    @Override
-    public void setAccess(PlayerAccess access) {
-        this.access = access;
-    }
-
     public void writeToBuffer(FriendlyByteBuf buffer) {
         buffer.writeBoolean(this.notified);
         buffer.writeUtf(DateUtils.toDateTimeString(this.getLastCardTime()));
@@ -199,6 +88,7 @@ public class PlayerTeleportData implements IPlayerTeleportData {
         buffer.writeNbt(this.getAccess().writeToNBT());
     }
 
+    @Override
     public void readFromBuffer(FriendlyByteBuf buffer) {
         this.notified = buffer.readBoolean();
         this.lastCardTime = DateUtils.format(buffer.readUtf());
@@ -221,17 +111,8 @@ public class PlayerTeleportData implements IPlayerTeleportData {
         }
 
         this.access = PlayerAccess.readFromNBT(Objects.requireNonNull(buffer.readNbt()));
-    }
 
-    public void copyFrom(IPlayerTeleportData capability) {
-        this.notified = capability.isNotified();
-        this.lastCardTime = capability.getLastCardTime();
-        this.lastTpTime = capability.getLastTpTime();
-        this.teleportCard.set(capability.getTeleportCard());
-        this.teleportRecords = capability.getTeleportRecords();
-        this.homeCoordinate = capability.getHomeCoordinate();
-        this.defaultHome = capability.getDefaultHome();
-        this.access = capability.getAccess();
+        this.save();
     }
 
     @Override
@@ -277,45 +158,219 @@ public class PlayerTeleportData implements IPlayerTeleportData {
     }
 
     @Override
-    public void deserializeNBT(CompoundTag nbt) {
+    public void deserializeNBT(CompoundTag nbt, boolean dirty) {
         this.notified = nbt.getBoolean("notified");
-        this.setLastCardTime(DateUtils.format(nbt.getString("lastCardTime")));
-        this.setLastTpTime(DateUtils.format(nbt.getString("lastTpTime")));
-        this.setTeleportCard(nbt.getInt("teleportCard"));
+        this.lastCardTime = DateUtils.format(nbt.getString("lastCardTime"));
+        this.lastTpTime = DateUtils.format(nbt.getString("lastTpTime"));
+        this.teleportCard.set(nbt.getInt("teleportCard"));
 
         // 反序列化传送记录
-        ListTag recordsNBT = nbt.getList("teleportRecords", 10); // 10 是 CompoundTag 的类型ID
+        ListTag recordsNBT = nbt.getList("teleportRecords", 10);
         List<TeleportRecord> records = new ArrayList<>();
         for (int i = 0; i < recordsNBT.size(); i++) {
             records.add(TeleportRecord.readFromNBT(recordsNBT.getCompound(i)));
         }
-        this.setTeleportRecords(records);
+        this.teleportRecords = records;
 
         // 反序列化家坐标
         ListTag homeCoordinateNBT = nbt.getList("homeCoordinate", 10);
-        Map<KeyValue<String, String>, Coordinate> homeCoordinate = new HashMap<>();
+        Map<KeyValue<String, String>, Coordinate> homeCoordinateMap = new HashMap<>();
         for (int i = 0; i < homeCoordinateNBT.size(); i++) {
             CompoundTag homeCoordinateTag = homeCoordinateNBT.getCompound(i);
-            homeCoordinate.put(new KeyValue<>(homeCoordinateTag.getString("key"), homeCoordinateTag.getString("value")),
+            homeCoordinateMap.put(new KeyValue<>(homeCoordinateTag.getString("key"), homeCoordinateTag.getString("value")),
                     Coordinate.readFromNBT(homeCoordinateTag.getCompound("coordinate")));
         }
-        this.setHomeCoordinate(homeCoordinate);
+        this.homeCoordinate = homeCoordinateMap;
 
         // 反序列化默认家
         ListTag defaultHomeNBT = nbt.getList("defaultHome", 10);
-        Map<String, String> defaultHome = new HashMap<>();
+        Map<String, String> defaultHomeMap = new HashMap<>();
         for (int i = 0; i < defaultHomeNBT.size(); i++) {
             CompoundTag defaultHomeTag = defaultHomeNBT.getCompound(i);
-            defaultHome.put(defaultHomeTag.getString("key"), defaultHomeTag.getString("value"));
+            defaultHomeMap.put(defaultHomeTag.getString("key"), defaultHomeTag.getString("value"));
         }
-        this.setDefaultHome(defaultHome);
+        this.defaultHome = defaultHomeMap;
 
         // 反序列化黑白名单
-        this.setAccess(PlayerAccess.readFromNBT(nbt.getCompound("access")));
+        this.access = PlayerAccess.readFromNBT(nbt.getCompound("access"));
+
+        if (dirty) {
+            this.save();
+        }
     }
 
     @Override
-    public void save(ServerPlayer player) {
-        player.getCapability(PlayerTeleportDataCapability.PLAYER_DATA).ifPresent(this::copyFrom);
+    public void copyFrom(PlayerTeleportData playerData) {
+        if (playerData == null) return;
+
+        this.notified = playerData.isNotified();
+        this.lastCardTime = playerData.getLastCardTime();
+        this.lastTpTime = playerData.getLastTpTime();
+        this.teleportCard.set(playerData.getTeleportCard());
+        this.teleportRecords = playerData.getTeleportRecords();
+        this.homeCoordinate = playerData.getHomeCoordinate();
+        this.defaultHome = playerData.getDefaultHome();
+        this.access = playerData.getAccess();
+
+        this.save();
     }
+
+    @Override
+    public void save() {
+        if (this.player instanceof ServerPlayer) {
+            PlayerDataManager.instance().put(player, serializeNBT());
+        }
+    }
+
+    // endregion override
+
+
+    private boolean notified;
+    private Date lastCardTime;
+    private Date lastTpTime;
+    private final AtomicInteger teleportCard = new AtomicInteger();
+    private List<TeleportRecord> teleportRecords;
+    /**
+     * dimension:name coordinate
+     */
+    private Map<KeyValue<String, String>, Coordinate> homeCoordinate;
+    /**
+     * dimension:name
+     */
+    private Map<String, String> defaultHome;
+    /**
+     * 玩家自定义的黑白名单
+     */
+    private PlayerAccess access;
+
+
+    public boolean isNotified() {
+        if (this.isDirty()) this.saveEx();
+        return notified;
+    }
+
+    public void setNotified(boolean notified) {
+        this.notified = notified;
+        this.save();
+    }
+
+    public @NonNull Date getLastCardTime() {
+        if (this.isDirty()) this.saveEx();
+        return this.lastCardTime == null ? this.lastCardTime = DateUtils.getDate(0, 1, 1) : this.lastCardTime;
+    }
+
+    public void setLastCardTime(Date time) {
+        this.lastCardTime = time;
+        this.save();
+    }
+
+    public @NonNull Date getLastTpTime() {
+        if (this.isDirty()) this.saveEx();
+        return this.lastTpTime == null ? this.lastTpTime = DateUtils.getDate(0, 1, 1) : this.lastTpTime;
+    }
+
+    public void setLastTpTime(Date time) {
+        this.lastTpTime = time;
+        this.save();
+    }
+
+    public int getTeleportCard() {
+        if (this.isDirty()) this.saveEx();
+        return this.teleportCard.get();
+    }
+
+    public void setTeleportCard(int num) {
+        this.teleportCard.set(num);
+        this.save();
+    }
+
+    public void plusTeleportCard(int num) {
+        this.setTeleportCard(this.getTeleportCard() + num);
+    }
+
+    public void subTeleportCard(int num) {
+        this.setTeleportCard(this.getTeleportCard() - num);
+    }
+
+    public @NonNull List<TeleportRecord> getTeleportRecords() {
+        if (this.isDirty()) this.saveEx();
+        return this.teleportRecords = CollectionUtils.isNullOrEmpty(this.teleportRecords) ? new ArrayList<>() : this.teleportRecords;
+    }
+
+    public @NonNull List<TeleportRecord> getTeleportRecords(EnumTeleportType type) {
+        if (this.isDirty()) this.saveEx();
+        return CollectionUtils.isNullOrEmpty(this.teleportRecords) ? this.teleportRecords = new ArrayList<>() :
+                this.teleportRecords.stream().filter(record -> record.getTeleportType() == type).collect(Collectors.toList());
+    }
+
+    public void setTeleportRecords(List<TeleportRecord> records) {
+        this.teleportRecords = records;
+        this.save();
+    }
+
+    public void addTeleportRecords(TeleportRecord... records) {
+        this.teleportRecords.addAll(Arrays.asList(records));
+        Arrays.stream(records).map(TeleportRecord::getTeleportTime).max(Date::compareTo).ifPresent(this::setLastTpTime);
+        this.save();
+    }
+
+    public Map<KeyValue<String, String>, Coordinate> getHomeCoordinate() {
+        if (this.isDirty()) this.saveEx();
+        return this.homeCoordinate = this.homeCoordinate == null ? new HashMap<>() : this.homeCoordinate;
+    }
+
+    public void setHomeCoordinate(Map<KeyValue<String, String>, Coordinate> homeCoordinate) {
+        this.homeCoordinate = homeCoordinate;
+        this.save();
+    }
+
+    public void addHomeCoordinate(KeyValue<String, String> key, Coordinate coordinate) {
+        this.getHomeCoordinate().put(key, coordinate);
+        this.save();
+    }
+
+    public Map<String, String> getDefaultHome() {
+        if (this.isDirty()) this.saveEx();
+        return this.defaultHome = this.defaultHome == null ? new HashMap<>() : this.defaultHome;
+    }
+
+    public void setDefaultHome(Map<String, String> defaultHome) {
+        this.defaultHome = defaultHome;
+        this.save();
+    }
+
+    public void addDefaultHome(String key, String value) {
+        this.getDefaultHome().put(key, value);
+        this.save();
+    }
+
+    public KeyValue<String, String> getDefaultHome(String dimension) {
+        if (this.getDefaultHome().containsKey(dimension)) {
+            return new KeyValue<>(dimension, this.getDefaultHome().get(dimension));
+        }
+        return null;
+    }
+
+    public PlayerAccess getAccess() {
+        if (this.isDirty()) this.saveEx();
+        return this.access = this.access == null ? new PlayerAccess() : this.access;
+    }
+
+    public void setAccess(PlayerAccess access) {
+        this.access = access;
+        this.save();
+    }
+
+
+    /**
+     * 同步玩家数据到客户端
+     */
+    public static void syncPlayerData(ServerPlayer player) {
+        // 创建自定义包并发送到客户端
+        PlayerDataSyncPacket packet = new PlayerDataSyncPacket(player.getUUID(), getData(player));
+        for (PlayerDataSyncPacket syncPacket : packet.split()) {
+            NarcissusUtils.sendPacketToPlayer(syncPacket, player);
+        }
+    }
+
 }
--- src/main/java/xin/vanilla/narcissus/event/ServerModEventHandler.java
+++ src/main/java/xin/vanilla/narcissus/event/ServerModEventHandler.java
@@ -1,22 +1,14 @@
 package xin.vanilla.narcissus.event;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xin.vanilla.narcissus.NarcissusFarewell;
 
 /**
  * 服务端 Mod事件处理器
  */
-@Mod.EventBusSubscriber(modid = NarcissusFarewell.MODID, value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.MOD)
+// @Mod.EventBusSubscriber(modid = NarcissusFarewell.MODID, value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ServerModEventHandler {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    @SubscribeEvent
-    public static void registerCaps(RegisterCapabilitiesEvent event) {
-        EventHandlerProxy.registerCaps(event);
-    }
 }
