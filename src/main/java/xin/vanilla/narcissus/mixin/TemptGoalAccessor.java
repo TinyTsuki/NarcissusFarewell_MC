9ff404dcea391c404c2edd97aa1e52814ae98793
辉小月 <TsukiMaaii@gmail.com>
2026-03-08 12:43:13 +0800

    fix(mixin): mixin未正确配置导致的问题

--- src/main/java/xin/vanilla/narcissus/mixin/TemptGoalAccessor.java
+++ src/main/java/xin/vanilla/narcissus/mixin/TemptGoalAccessor.java
@@ -5,8 +5,8 @@ import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(value = TemptGoal.class, remap = false)
+@Mixin(TemptGoal.class)
 public interface TemptGoalAccessor {
-    @Accessor(value = "player")
+    @Accessor("player")
     Player narcissus$player();
 }
