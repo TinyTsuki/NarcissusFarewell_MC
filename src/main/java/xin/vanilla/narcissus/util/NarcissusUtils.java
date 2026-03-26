55ce0050d052c2c42678360a016f3711bc30510c
辉小月 <TsukiMaaii@gmail.com>
2026-03-26 22:22:00 +0800

    refactor(utils): 使用DimensionUtils替代硬编码坐标边界

--- src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
+++ src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
@@ -550,14 +550,6 @@ public class NarcissusUtils {
 
     // region 安全坐标
 
-    public static int getWorldMinY(Level world) {
-        return world.getMinBuildHeight();
-    }
-
-    public static int getWorldMaxY(Level world) {
-        return world.getMaxBuildHeight();
-    }
-
     public static SafeWorldCoordinate findTopCandidate(ServerLevel world, SafeWorldCoordinate start) {
         return new SafeCoordinateFinder(world).findTopCandidate(start);
     }
