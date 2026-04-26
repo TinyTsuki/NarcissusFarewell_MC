003524d0132049d7a1d302e573d3c79750703c88
辉小月 <TsukiMaaii@gmail.com>
2026-04-26 13:34:22 +0800

    refactor(network): 重构网络包实现以统一接口

--- src/main/java/xin/vanilla/narcissus/network/packet/TpGraveToServer.java
+++ src/main/java/xin/vanilla/narcissus/network/packet/TpGraveToServer.java
@@ -3,21 +3,14 @@ package xin.vanilla.narcissus.network.packet;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
-import net.minecraftforge.fml.network.simple.SimpleChannel;
-import xin.vanilla.banira.common.network.NetworkPacket;
 import xin.vanilla.banira.common.util.CommandUtils;
 import xin.vanilla.narcissus.enums.EnumCommandType;
-import xin.vanilla.narcissus.network.NetworkInit;
+import xin.vanilla.narcissus.network.NetworkPacket;
 import xin.vanilla.narcissus.util.NarcissusUtils;
 
 import java.util.function.Supplier;
 
 public class TpGraveToServer implements NetworkPacket {
-
-    @Override
-    public Supplier<SimpleChannel> channel() {
-        return () -> NetworkInit.INSTANCE;
-    }
 
     public TpGraveToServer() {
     }
