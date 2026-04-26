ce820db95816cb1d517982f5b2475b1d63a25792
辉小月 <TsukiMaaii@gmail.com>
2026-04-26 13:34:22 +0800

    refactor(network): 重构网络包实现以统一接口

--- src/main/java/xin/vanilla/narcissus/network/packet/WaypointAddStageToServer.java
+++ src/main/java/xin/vanilla/narcissus/network/packet/WaypointAddStageToServer.java
@@ -4,12 +4,10 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.simple.SimpleChannel;
-import xin.vanilla.banira.common.network.NetworkPacket;
 import xin.vanilla.banira.common.util.CommandUtils;
 import xin.vanilla.banira.common.util.StringUtils;
 import xin.vanilla.narcissus.config.CommonConfig;
-import xin.vanilla.narcissus.network.NetworkInit;
+import xin.vanilla.narcissus.network.NetworkPacket;
 import xin.vanilla.narcissus.util.NarcissusUtils;
 
 import java.util.function.Supplier;
@@ -17,11 +15,6 @@ import java.util.function.Supplier;
 @Getter
 @Accessors(fluent = true)
 public class WaypointAddStageToServer implements NetworkPacket {
-
-    @Override
-    public Supplier<SimpleChannel> channel() {
-        return () -> NetworkInit.INSTANCE;
-    }
 
     private static final int MAX_NAME_LEN = 64;
     private static final int MAX_DIMENSION_LEN = 256;
