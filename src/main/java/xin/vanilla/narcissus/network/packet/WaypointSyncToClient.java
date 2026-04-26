804e6648e8ab95ca40997b65877eece493ca302d
辉小月 <TsukiMaaii@gmail.com>
2026-04-26 13:34:22 +0800

    refactor(network): 重构网络包实现以统一接口

--- src/main/java/xin/vanilla/narcissus/network/packet/WaypointSyncToClient.java
+++ src/main/java/xin/vanilla/narcissus/network/packet/WaypointSyncToClient.java
@@ -6,11 +6,9 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.network.NetworkEvent;
-import net.minecraftforge.fml.network.simple.SimpleChannel;
-import xin.vanilla.banira.common.network.NetworkPacket;
 import xin.vanilla.narcissus.data.SafeWorldCoordinate;
 import xin.vanilla.narcissus.integration.MapHelper;
-import xin.vanilla.narcissus.network.NetworkInit;
+import xin.vanilla.narcissus.network.NetworkPacket;
 
 import java.util.function.Supplier;
 
@@ -18,11 +16,6 @@ import java.util.function.Supplier;
 @Getter
 @Accessors(fluent = true)
 public class WaypointSyncToClient implements NetworkPacket {
-
-    @Override
-    public Supplier<SimpleChannel> channel() {
-        return () -> NetworkInit.INSTANCE;
-    }
 
     public enum Action {
         ADD,
