599366b2337bae12e503e175c2325033dfa8ae94
辉小月 <TsukiMaaii@gmail.com>
2026-07-13 09:39:36 +0800

    Adapt Banira migration to Forge 1.20.1

--- src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
+++ src/main/java/xin/vanilla/narcissus/util/NarcissusUtils.java
@@ -33,7 +33,6 @@ import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xin.vanilla.banira.BaniraCodex;
 import xin.vanilla.banira.common.data.KeyValue;
 import xin.vanilla.banira.common.util.*;
 import xin.vanilla.banira.common.util.CommandUtils;
@@ -481,7 +480,11 @@ public class NarcissusUtils {
     // region 安全坐标
 
     public static ServerLevel getServerLevel() {
-        return BaniraCodex.serverInstance().key().getAllLevels().iterator().next();
+        MinecraftServer server = BaniraServerUtils.currentServer();
+        if (server == null) {
+            throw new IllegalStateException("Minecraft server is not ready");
+        }
+        return server.getAllLevels().iterator().next();
     }
 
     public static SafeWorldCoordinate findTopCandidate(ServerPlayer player, SafeWorldCoordinate start) {
