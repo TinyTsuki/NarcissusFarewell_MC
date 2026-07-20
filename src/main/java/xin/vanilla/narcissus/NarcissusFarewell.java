21e221bb7f9f1dbee4e002e1e7da93ba6f39b6df
辉小月 <TsukiMaaii@gmail.com>
2026-07-20 10:25:33 +0800

    Add dedicated network persistence smoke

--- src/main/java/xin/vanilla/narcissus/NarcissusFarewell.java
+++ src/main/java/xin/vanilla/narcissus/NarcissusFarewell.java
@@ -34,6 +34,7 @@ import xin.vanilla.narcissus.event.ClientModEventHandler;
 import xin.vanilla.narcissus.event.EventHandlerProxy;
 import xin.vanilla.narcissus.integration.ScreenHelper;
 import xin.vanilla.narcissus.internal.forge.event.ForgeNarcissusGameEventAdapter;
+import xin.vanilla.narcissus.internal.server.dev.NarcissusNetworkSmokeServerRunner;
 import xin.vanilla.narcissus.network.NetworkInit;
 import xin.vanilla.narcissus.network.packet.CostConfigSyncToClient;
 import xin.vanilla.narcissus.network.packet.StageDataSyncToClient;
@@ -74,6 +75,7 @@ public class NarcissusFarewell {
         BaniraEventBus.Server.onStopping(server -> PlayerTeleportData.clear());
         BaniraEventBus.Server.onTick(event -> EventHandlerProxy.onServerTick());
         ForgeNarcissusGameEventAdapter.register();
+        NarcissusNetworkSmokeServerRunner.register();
 
         BaniraEventBus.ModLifecycle.onCommonSetup(event -> {
             NarcissusNotificationTypes.registerAllOnServer();
