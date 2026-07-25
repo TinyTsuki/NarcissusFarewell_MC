ea3d376699e7eb2e25cc2cadbdd554b9644bd2d6
辉小月 <TsukiMaaii@gmail.com>
2026-07-25 10:23:10 +0800

    Align player preferences with Aotake screen

--- src/main/java/xin/vanilla/narcissus/internal/client/dev/NarcissusUiSmokeRunner.java
+++ src/main/java/xin/vanilla/narcissus/internal/client/dev/NarcissusUiSmokeRunner.java
@@ -21,6 +21,7 @@ import xin.vanilla.narcissus.network.packet.PlayerConfigSyncToServer;
 import xin.vanilla.narcissus.network.packet.WaypointAddHomeToServer;
 import xin.vanilla.narcissus.network.packet.WaypointDelToServer;
 import xin.vanilla.narcissus.network.packet.WaypointTeleportToServer;
+import xin.vanilla.narcissus.screen.PlayerConfigScreen;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
@@ -67,6 +68,8 @@ public final class NarcissusUiSmokeRunner {
         this.outputDir = outputDir;
         this.options = options;
         this.preWorldSteps = Arrays.asList(
+                new ScreenStep("player-preferences", true, client -> client.setScreen(
+                        new PlayerConfigScreen(new PlayerConfigScreen.Args()))),
                 new ScreenStep("client-config", false, client -> client.setScreen(
                         new ConfigEditorScreen(ClientConfig.get().holder(), new ConfigEditorScreen.Args()))),
                 new ScreenStep("common-config", true, client -> client.setScreen(
